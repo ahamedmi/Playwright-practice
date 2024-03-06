d84773665298c62b5882b90e9a883e02fc28e9f3
Muzakkeen Infas Ahamed <66883031+ahamedmi@users.noreply.github.com>
2024-03-06 09:21:37 +0530

    Add files via upload

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,38 +1,38 @@
 const { test, expect } = require("@playwright/test");
-/* test("first test",async ({page})=>{
+test("first test",async ({page})=>{
     await page.goto("https://playwright.dev");
     await page.pause();
-}) */
+}) 
 
-/* test("Click element",async ({page})=>{
+ test("Click element",async ({page})=>{
    await page.goto("https://the-internet.herokuapp.com")
    await page.locator("text=Add/Remove Elements").click() //create locator using text based
    await page.locator("text=Add Element").click()
-}) */
+}) 
 
-/* test("click a element",async ({page})=>{
+test("click a element",async ({page})=>{
     await page.goto("https://the-internet.herokuapp.com")
     await page.click("text=Add/Remove Elements")
     await page.click("text=Add Element")
     await page.pause();
-}) */
+}) 
 
 //group tests 
 
-/* test.describe.parallel("smoke test", () => {
+test.describe.parallel("smoke test", () => {
 
     test.beforeEach(async ({page})=>{
         await page.goto("/");
     })
     test.afterEach(async ()=>{
         console.log("this test is finished")
-    }) */
+    })
 
-/*   test("click a element test_1 @smoke-regression", async ({ page }) => {
+ test("click a element test_1 @smoke-regression", async ({ page }) => {
     
-  }) */
+  })
 
-/*  test("click a element test_2 @smoke", async ({ page }) => {
+test("click a element test_2 @smoke", async ({ page }) => {
      const addRemoveElements = page.locator("text=Add/Remove Elements");
      const addElement = page.locator("text=Add Element");
      await addRemoveElements.click();
@@ -50,12 +50,12 @@ const { test, expect } = require("@playwright/test");
      await page.click("text=Add Element")
      await page.goto("/checkboxes")
      await page.pause();
- }) */
-//})
+ }) 
+})
 
-/* test("click a element test_1 @smoke-regression",async ({page})=>{
+test("click a element test_1 @smoke-regression",async ({page})=>{
     await page.goto("https://the-internet.herokuapp.com");
-    //await page.pause();
+    await page.pause();
 })
 
 test("click a element test_2 @smoke",async ({page})=>{
@@ -78,15 +78,15 @@ test("Click element test_3 @regression-smoke",async ({page})=>{
     await page.click("text=Add/Remove Elements")
     await page.click("text=Add Element")
     //await page.pause();
-}) */
+}) 
 
 test.describe.parallel("Hovering Element", () => {
 
-    /*  test.use({
+    test.use({
          storageState :'authentication.json',
          viewport : {width:600,height:800}
      })
-  */
+
     test.beforeEach(async ({ page }) => {
         await page.goto('https://the-internet.herokuapp.com/key_presses?');
     })
@@ -97,7 +97,7 @@ test.describe.parallel("Hovering Element", () => {
 
     })
 
-    /*  test("dropdown with label",async ({page})=>{
+    test("dropdown with label",async ({page})=>{
          //test.setTimeout(10000);
          await page.locator('#dropdown').selectOption({label : 'Option 1'})
          await expect(page.locator('#dropdown')).toHaveValue('1')
@@ -107,9 +107,9 @@ test.describe.parallel("Hovering Element", () => {
  
          await page.pause();
  
-     }) */
+     }) 
 
-    /*  test.skip("test 2",async ({page})=>{
+ test.skip("test 2",async ({page})=>{
          
          await page.locator('#leftPanel').getByRole('link', { name: 'Transfer Funds' }).click();
          await page.locator('#amount').click();
@@ -139,7 +139,7 @@ test.describe.parallel("Hovering Element", () => {
          await page.locator('input[name="amount"]').click();
          await page.locator('input[name="amount"]').fill('14589');
          await page.getByRole('button', { name: 'Send Payment' }).click();  
-     }) */
+     })
 
 })
 
